8b39bbedd65e480645dea82b2b209018fb60d7c7
Mattangeelo <matheus.familiacristan@hotmail.com>
2024-02-02 13:21:13 -0300

    02-02-2024
    implementando categorias de produtos

--- app/Controllers/Admin/Categorias.php
+++ app/Controllers/Admin/Categorias.php
@@ -4,10 +4,44 @@ namespace App\Controllers\Admin;
 
 use App\Controllers\BaseController;
 
+
 class Categorias extends BaseController
 {
+    private $categoriaModel;
+
+    public function __construct(){
+        $this->categoriaModel= new \App\Models\CategoriaModel();
+    }
     public function index()
     {
-        //
+        
+        $data = [
+            'titulo'=> 'Listando as Categorias',
+            'categorias' => $this->categoriaModel->withDeleted(true)->paginate(10),
+            'pager' => $this->categoriaModel->pager
+        ];
+
+        return view('Admin/Categorias/index',$data);
+    }
+
+    public function procurar(){
+
+        if(!$this->request->isAJAX()){
+            exit('Pagina não encontrada');
+        }
+
+        $categorias =  $this->categoriaModel->procurar($this->request->getGet('term'));
+
+        $retorno = [];
+
+        foreach($categorias as $categoria){
+            $data['id'] = $categoria->id;
+            $data['value'] = $categoria->nome;
+            
+            $retorno[] = $data;
+        }
+
+        return $this->response->setJSON($retorno);
+        
     }
 }
--- app/Models/CategoriaModel.php
+++ app/Models/CategoriaModel.php
@@ -4,38 +4,59 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
-class Categoria extends Model
+class CategoriaModel extends Model
 {
-    protected $DBGroup          = 'default';
     protected $table            = 'categorias';
-    protected $primaryKey       = 'id';
-    protected $useAutoIncrement = true;
-    protected $returnType       = 'array';
-    protected $useSoftDeletes   = false;
-    protected $protectFields    = true;
-    protected $allowedFields    = [];
+    protected $returnType       = 'App\Entities\Categoria';
+    protected $useSoftDeletes   = true;
+    protected $allowedFields    = ['nome','ativo','slug'];
 
     // Dates
-    protected $useTimestamps = false;
+    protected $useTimestamps = true;
     protected $dateFormat    = 'datetime';
-    protected $createdField  = 'created_at';
-    protected $updatedField  = 'updated_at';
-    protected $deletedField  = 'deleted_at';
+    protected $createdField  = 'criado_em';
+    protected $updatedField  = 'atualizado_em';
+    protected $deletedField  = 'deletado_em';
 
     // Validation
-    protected $validationRules      = [];
-    protected $validationMessages   = [];
-    protected $skipValidation       = false;
-    protected $cleanValidationRules = true;
+    protected $validationRules = [
+        'nome'     => 'required|max_length[120]|min_length[2]|is_unique[categorias.nome]',
+    ];
+    protected $validationMessages = [
+        'nome' => [
+            'required' => 'O campo Nome é Obrigatório.',
+        ],
+    ];
+    //Eventos Callback
+    protected $beforeInsert = ['criaSlug'];
+    protected $beforeUpdate = ['criaSlug'];
 
-    // Callbacks
-    protected $allowCallbacks = true;
-    protected $beforeInsert   = [];
-    protected $afterInsert    = [];
-    protected $beforeUpdate   = [];
-    protected $afterUpdate    = [];
-    protected $beforeFind     = [];
-    protected $afterFind      = [];
-    protected $beforeDelete   = [];
-    protected $afterDelete    = [];
+    protected function criaSlug(array $data){
+
+        if(isset($data['data']['nome'])){
+
+            $data['data']['slug'] = mb_url_title($data['data']['nome'], '-',TRUE);
+
+        }
+
+        return $data;
+    }
+
+    public function procurar($term) {
+        if($term === null){
+            return [];
+        }
+
+        return $this->select('id, nome')
+                     ->like('nome', $term)
+                     ->withDeleted(true)
+                     ->get()
+                     ->getResult();
+    }
+
+    public function desabilitaValidacaoSenha(){
+
+        unset($this->validationRules['password']);
+        unset($this->validationRules['password_confirmation']);
+    }
 }
